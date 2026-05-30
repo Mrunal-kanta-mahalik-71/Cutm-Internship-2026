4b4e2d91e5a6c58e1d8a10ccc5ef8137d3336099
Mrunal-kanta-mahalik-71 <rajababuofficial2004@gmail.com>
2026-05-31 00:46:56 +0530

    update

---
+++ First_task/First_task/Size_Range.java
@@ -0,0 +1,22 @@
+package First_task;
+public class Size_Range 
+{
+    public static void main(String [] arg)
+    {
+        System.out.println("Size of byte :"+ Byte.SIZE +" bits");
+        System.out.println("Range of Byte is "+Byte.MIN_VALUE+" to "+ Byte.MAX_VALUE);
+        System.out.println("Size of byte :"+ Short.SIZE +" bits");
+        System.out.println("Range of Byte is "+Short.MIN_VALUE+" to "+ Short.MAX_VALUE);
+        System.out.println("Size of byte :"+ Long.SIZE +" bits");
+        System.out.println("Range of Byte is "+Long.MIN_VALUE+" to "+ Long.MAX_VALUE);
+        System.out.println("Size of byte :"+ Character.SIZE +" bits");
+        System.out.println("Range of Byte is "+Character.MIN_VALUE+" to "+ Character.MAX_VALUE);
+        System.out.println("Size of byte :"+ Integer.SIZE +" bits");
+        System.out.println("Range of Byte is "+Integer.MIN_VALUE+" to "+ Integer.MAX_VALUE);
+        System.out.println("Size of byte :"+ Float.SIZE +" bits");
+        System.out.println("Range of Byte is "+Float.MIN_VALUE+" to "+ Float.MAX_VALUE);
+        System.out.println("Size of byte :"+ Double.SIZE +" bits");
+        System.out.println("Range of Byte is "+Double.MIN_VALUE+" to "+ Double.MAX_VALUE);
+
+    }
+}
