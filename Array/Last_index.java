276e8efb6898488990565e44ac88ee141edb588d
Mrunal-kanta-mahalik-71 <rajababuofficial2004@gmail.com>
2026-06-06 23:12:12 +0530

    Array

---
+++ Array/Last_index.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class Last_index
+{
+
+	public static void main(String[] args) {
+		 int[] arr = {1, 2, 9, 3, 9, 4, 9, 6, 7, 8};
+
+	        int element = 9;
+	        int lastIndex = -1;
+
+	        for (int i = 0; i < arr.length; i++) {
+
+	            if (arr[i] == element) {
+	                lastIndex = i;
+	            }
+	        }
+
+	        System.out.println("Last index of " + element + " is: " + lastIndex);
+	}
+
+}
