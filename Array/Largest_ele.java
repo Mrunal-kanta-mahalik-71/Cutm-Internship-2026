208b827047f1e5e8ffebe6985e6346dde54008b1
Mrunal-kanta-mahalik-71 <rajababuofficial2004@gmail.com>
2026-06-09 12:04:56 +0530

    Array

---
+++ Array/Largest_ele.java
@@ -0,0 +1,31 @@
+package Array;
+import java.util.Scanner;
+public class Largest_ele {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter arrys size: ");
+		int n= sc.nextInt();
+		int arr[]= new int [n];
+		System.out.print("Enter arrys element: ");
+		for(int i=0;i<n; i++)
+		{
+			
+			arr[i]= sc.nextInt();
+		}
+		largest(arr,n);
+	}
+	public static void largest(int arr[] , int n)
+	{
+		int max=0;
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i] > max)
+			{
+				max= arr[i];
+			}
+		}
+		System.out.println("Largest element is : "+ max);
+	}
+
+}
---
+++ Array/Squre_ele.java
@@ -0,0 +1,28 @@
+package Array;
+import java.util.Scanner;
+public class Squre_ele {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter arrys size: ");
+		int n= sc.nextInt();
+		int arr[]= new int [n];
+		System.out.print("Enter arrys element: ");
+		for(int i=0;i<n; i++)
+		{
+			
+			arr[i]= sc.nextInt();
+		}
+		
+		squire(arr,n);
+	}
+	public static void squire(int arr[] , int n)
+	{
+		System.out.print("Squire of elements : "+ "  ");
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(arr[i]* arr[i] +"   ");
+		}
+	}
+
+}
